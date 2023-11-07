774c167d7c90b0704762b3f9e6b1e66881aebabf
arriqaaq <farhan.khan@surrealdb.com>
2023-11-07 20:10:19 +0530

    chore: add tests for s3fifo

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -25,6 +25,8 @@ impl<K, V> Entry<K, V> {
     }
 }
 
+/// Cache is an implementation of "S3-FIFO" from "FIFO Queues are ALL You Need for Cache Eviction" by
+/// Juncheng Yang, et al: https://jasony.me/publication/sosp23-s3fifo.pdf
 pub struct Cache<K, V>
 where
     K: PartialEq,
@@ -41,7 +43,7 @@ where
     K: PartialEq,
 {
     pub fn new(cache_size: usize) -> Self {
-        assert!(cache_size >= 10);
+        assert!(cache_size > 0);
         let min_eviction_size = cache_size / 10;
 
         Self {
@@ -127,9 +129,91 @@ where
                     self.insert_m(tail);
                 } else {
                     self.insert_g(tail);
-                    evicted = false;
+                    evicted = true;
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Debug;
+    use std::sync::atomic::AtomicUsize;
+    use std::sync::atomic::Ordering::SeqCst;
+
+    use super::*;
+
+    fn assert_opt_eq<V: PartialEq + Debug>(opt: Option<&V>, v: V) {
+        assert!(opt.is_some());
+        assert_eq!(opt.unwrap(), &v);
+    }
+
+    #[test]
+    fn test_insert_and_read() {
+        let mut cache = Cache::new(2);
+
+        cache.insert("apple", "red");
+        cache.insert("banana", "yellow");
+
+        assert_opt_eq(cache.read(&"apple"), "red");
+        assert_opt_eq(cache.read(&"banana"), "yellow");
+    }
+
+    #[test]
+    fn test_insert_removes_oldest() {
+        let mut cache = Cache::new(2);
+
+        cache.insert("apple", "red");
+        cache.insert("banana", "yellow");
+        cache.insert("orange", "orange");
+
+        assert!(cache.read(&"apple").is_none());
+        assert_opt_eq(cache.read(&"banana"), "yellow");
+        assert_opt_eq(cache.read(&"orange"), "orange");
+
+        // "apple" should been removed from the cache.
+        cache.insert("apple", "orange");
+        cache.insert("tomato", "red");
+
+        assert!(cache.read(&"orange").is_none());
+        assert_opt_eq(cache.read(&"apple"), "orange");
+        assert_opt_eq(cache.read(&"tomato"), "red");
+    }
+
+    #[test]
+    fn test_send() {
+        use std::thread;
+
+        let mut cache = Cache::new(2);
+        cache.insert(1, "a");
+
+        let handle = thread::spawn(move || {
+            assert_eq!(cache.read(&1), Some(&"a"));
+        });
+
+        assert!(handle.join().is_ok());
+    }
+
+    #[test]
+    fn test_no_memory_leaks() {
+        static DROP_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+        struct DropCounter;
+
+        impl Drop for DropCounter {
+            fn drop(&mut self) {
+                DROP_COUNT.fetch_add(1, SeqCst);
+            }
+        }
+
+        let n = 100;
+        for _ in 0..n {
+            let mut cache = Cache::new(2);
+            for i in 0..n {
+                cache.insert(i, DropCounter {});
+            }
+        }
+        assert_eq!(DROP_COUNT.load(SeqCst), n * n);
     }
 }
