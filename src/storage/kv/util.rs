602abccafaaf835668f750ccfccc7356ea870e26
Sergii Glushchenko <sergii.glushchenko@surrealdb.com>
2024-09-17 10:39:39 +0100

    Savepoint rollback on an updated key should roll it back to previous version (#77)

--- src/storage/kv/util.rs
+++ src/storage/kv/util.rs
@@ -4,23 +4,10 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use bytes::Bytes;
 use chrono::Utc;
-use crc32fast::Hasher as crc32Hasher;
-use sha2::{Digest, Sha256};
 use vart::VariableSizeKey;
 
 use crate::Result;
-
-/// Calculates the CRC32 hash of a byte array.
-/// It creates a new CRC32 hasher, updates it with the byte array, and finalizes the hash.
-/// It returns the hash as a 32-bit unsigned integer.
-#[allow(unused)]
-pub(crate) fn calculate_crc32(buf: &[u8]) -> u32 {
-    let mut hasher = crc32Hasher::new();
-    hasher.update(buf);
-    hasher.finalize()
-}
 
 /// Gets the current time in nanoseconds since the Unix epoch.
 /// It gets the current time in UTC, extracts the timestamp in nanoseconds, and asserts that the timestamp is positive.
@@ -30,13 +17,6 @@ pub(crate) fn now() -> u64 {
     let timestamp = utc_now.timestamp_nanos_opt().unwrap();
     assert!(timestamp > 0);
     timestamp as u64
-}
-
-pub(crate) fn sha256(arg: Bytes) -> Bytes {
-    let mut hasher = Sha256::new();
-    hasher.update(arg);
-    let result = hasher.finalize();
-    Bytes::copy_from_slice(result.as_slice())
 }
 
 pub(crate) fn sanitize_directory(directory: &str) -> std::io::Result<PathBuf> {
