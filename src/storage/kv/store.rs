4d134eccf509d1cb04392c960219a15f3a17830d
Sergii Glushchenko <sergii.glushchenko@surrealdb.com>
2024-11-15 02:11:21 +0100

    perf: Do not hold write lock while updating the index (#129)

--- src/storage/kv/store.rs
+++ src/storage/kv/store.rs
@@ -652,13 +652,13 @@ impl Core {
     where
         F: Fn(&Entry) -> IndexValue,
     {
-        let mut index = self.indexer.write();
+        let mut new_index = self.indexer.read().clone();
 
         for entry in &task.entries {
             // If the entry is marked as deleted, delete it.
             if let Some(metadata) = entry.metadata.as_ref() {
                 if metadata.is_deleted() {
-                    index.delete(&mut entry.key[..].into());
+                    new_index.delete(&mut entry.key[..].into());
                     continue;
                 }
             }
@@ -666,7 +666,7 @@ impl Core {
             let index_value = encode_entry(entry);
 
             if self.opts.enable_versions {
-                index.insert(
+                new_index.insert(
                     &mut entry.key[..].into(),
                     index_value,
                     task.tx_id,
@@ -674,7 +674,7 @@ impl Core {
                     true,
                 )?;
             } else {
-                index.insert_or_replace(
+                new_index.insert_or_replace(
                     &mut entry.key[..].into(),
                     index_value,
                     task.tx_id,
@@ -683,6 +683,8 @@ impl Core {
                 )?;
             }
         }
+
+        *self.indexer.write() = new_index;
 
         Ok(())
     }
