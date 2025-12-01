ec7bd8ac8684517dfea06973570746ecc5c51923
arriqaaq <farhan.khan@surrealdb.com>
2025-12-01 15:42:53 +0530

    fixes

--- src/wal/mod.rs
+++ src/wal/mod.rs
@@ -1,22 +1,3 @@
-//  Copyright (c) 2024 SurrealDB Ltd.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
-//! Write-Ahead Log (WAL) implementation.
-//!
-//! This module provides a Write-Ahead Log implementation based on RocksDB's design,
-//! with support for record fragmentation, compression, and recovery.
-
 use std::fmt;
 use std::fs::{self, read_dir, File};
 use std::io::{self, BufWriter, Write};
