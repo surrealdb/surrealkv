7f87dca49d683ea436a6e1bcd1532d0f6be0a248
arriqaaq <farhan.khan@surrealdb.com>
2025-12-12 18:02:52 +0530

    fix comments

--- src/lib.rs
+++ src/lib.rs
@@ -210,7 +210,7 @@ pub struct Options {
 
 	// Shutdown configuration
 	/// If true, flush active memtable to SSTable during shutdown.
-	/// If false, skip flush for faster shutdown (unpersisted data will be lost if WAL disabled).
+	/// If false, skip flush for faster shutdown.
 	///
 	/// DEFAULT: false
 	pub flush_on_close: bool,
